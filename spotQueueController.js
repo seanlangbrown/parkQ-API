d432c736eedda18d4ee3aa52c5840a0f7ea432e4
Seanlangbrown <seanlangbrown@gmail.com>
2018-07-03 18:38:26 -0700

    implement controller methods for tracking assigned spots and filling spots

--- spotQueueController.js
+++ spotQueueController.js
@@ -40,6 +40,29 @@ class Queue extends Object {
 		return dequed.data;
 	}
 
+	toArray () {
+		let arr = [];
+		for (var i = 0; i < this.length; i++) {
+			let spot = this.get();
+			arr.push(spot);
+			this.put(spot);
+		}
+		return arr;
+	}
+
+	find (property, value) {
+		let foundItem = null;
+		for (var i = 0; i < this.length; i++) {
+			let item = this.get();
+			if (item[property] === value) {
+				foundItem = item;
+			} else {
+				this.put(item);
+			}
+		}
+		return foundItem;
+	}
+
 }
 
 
@@ -58,7 +81,14 @@ class Spot extends Object {
 		this.assignedAt = Date.now();
 	}
 
+	fill () {
+		this.empty = false;
+	}
 
+	release () {
+		this.empty = true;
+		this.assignedAt = null;
+	}
 
 	json() {
 		return {
@@ -70,7 +100,7 @@ class Spot extends Object {
 }
 
 const spotQueue = new Queue();
-
+const assignedSpots = {};
 
 module.exports.assign = function() {
 	let openSpot = spotQueue.get()
@@ -78,22 +108,35 @@ module.exports.assign = function() {
 		return {};
 	}
 	openSpot.assign(); //300, spotQueue);
-	//assignedSpots[openSpot.id] = openSpot;
+	assignedSpots[openSpot.id] = openSpot;
 	return openSpot.json();
 }
 
 module.exports.view = function() {
-	let emptySpots = [];
-	for (var i = 0; i < spotQueue.length; i++) {
-		let spot = spotQueue.get();
-		emptySpots.push(spot);
-		spotQueue.put(spot);
-	}
+	let emptySpots = spotQueue.toArray();
 	return emptySpots;
+}
+
+module.exports.count = function() {
+	return spotQueue.length;
+}
+
+module.exports.take = function(id) {
+	if (assignedSpots[id] === undefined) {
+		assignedSpots[id] = spotQueue.find("id", id); //TODO: error handling!
+	}
+	assignedSpots[id].fill();
 }
 
 
 module.exports.release = function(id) {
-	let openSpot = new Spot(id);
+	let openSpot;
+	if (assignedSpots[id] !== undefined) {
+		openSpot = assignedSpots[id];
+		delete assignedSpots[id];
+	} else {
+		openSpot = new Spot(id);
+	}
+	openSpot.release();
 	spotQueue.put(openSpot);
 }
